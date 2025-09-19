107030aaaecddb722de27c7ea4592bcbc8ebf2b4
webdevstudio-lab <webdevstudio2023@gmail.com>
2025-09-19 18:08:57 +0000

    Implement eslin & prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from aqusitions ! ");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from aqusitions ! ');
 });
 
 export default app;
